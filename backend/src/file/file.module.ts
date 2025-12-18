a607fa99ecb3c2404a882e2e7f6d9ad02af79618
Nikita Morkovkin <nikitamorkovkinwork@gmail.com>
2025-12-18 19:22:44 +0500

    ✨ feat(backend, db):
    - Added file functional

--- backend/src/file/file.module.ts
+++ backend/src/file/file.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
-import { FileService } from './file.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import appRootPath from 'app-root-path';
+import { join } from 'path';
 import { FileController } from './file.controller';
+import { FileService } from './file.service';
 
 @Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(String(appRootPath), 'uploads'),
+      serveRoot: '/uploads',
+    }),
+  ],
   controllers: [FileController],
   providers: [FileService],
 })
--- backend/src/file/file.service.ts
+++ backend/src/file/file.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
-
+import appRootPath from 'app-root-path';
+import { ensureDir, writeFile } from 'fs-extra';
+import { join } from 'path';
+import { FileResponse } from './file.interface';
 @Injectable()
-export class FileService {}
+export class FileService {
+  async saveFiles(files: Express.Multer.File[], folder: string = 'products') {
+    const uploadedFolder = join(String(appRootPath), 'uploads', folder);
+
+    await ensureDir(uploadedFolder);
+
+    const response: FileResponse[] = await Promise.all(
+      files.map(async (file) => {
+        const originalName = `${Date.now()}-${file.originalname}`;
+        await writeFile(join(uploadedFolder, originalName), file.buffer);
+
+        return {
+          url: `uploads/${folder}/${originalName}`,
+          name: originalName,
+        };
+      }),
+    );
+
+    return response;
+  }
+}
